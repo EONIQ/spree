c76a49803163adf952b76e9649d5ada4990aeab0
charleswong28 <johndoe@example.com>
2018-01-18 12:55:07 +0800

    Use friendly find instead of find_by_slug, after adding spree-globalize, when slug is translated, slug is not an attribute of product

--- backend/app/controllers/spree/admin/product_properties_controller.rb
+++ backend/app/controllers/spree/admin/product_properties_controller.rb
@@ -1,11 +1,17 @@
 module Spree
   module Admin
     class ProductPropertiesController < ResourceController
-      belongs_to 'spree/product', :find_by => :slug
+      # belongs_to 'spree/product', :find_by => :slug
+      before_action :load_data
+
       before_action :find_properties
       before_action :setup_property, only: :index
 
       private
+        def load_data
+          @product = Spree::Product.friendly.find(params[:product_id])
+        end
+
         def find_properties
           @properties = Spree::Property.pluck(:name)
         end
